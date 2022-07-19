cdad4a4766ee395f87eb25340be51f61a12e06e6
MuruGeshhh <108484042+MuruGeshhh@users.noreply.github.com>
2022-07-19 11:35:36 +0800

    added date and time

--- src/main/java/sg/edu/nus/iss/day12wkshp/indexResource.java
+++ src/main/java/sg/edu/nus/iss/day12wkshp/indexResource.java
@@ -1,9 +1,13 @@
 package sg.edu.nus.iss.day12wkshp;
 
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Date;
+import java.util.Calendar;;
 
 
 
@@ -15,7 +19,14 @@ public class indexResource {
     @GetMapping(produces = {"text/html"})
     public String index(Model model) {
 
-        return "indexresource";
+        model.addAttribute("currTime", (new Date().toString()));
+
+        Calendar cal = Calendar.getInstance();
+        model.addAttribute("currHour",cal.get(Calendar.HOUR_OF_DAY));
+
+
+
+        return "indexresource"; // MUST BE THE same as html name 
    }
 
 
