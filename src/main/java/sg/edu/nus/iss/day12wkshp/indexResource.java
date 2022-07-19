9c61f130d2c32816d9573062ac52bbd445d4240c
MuruGeshhh <108484042+MuruGeshhh@users.noreply.github.com>
2022-07-19 11:00:15 +0800

    added indexresource

---
+++ src/main/java/sg/edu/nus/iss/day12wkshp/indexResource.java
@@ -0,0 +1,27 @@
+package sg.edu.nus.iss.day12wkshp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping(path= {"/indexresource", "/indexresource.html"})
+
+public class indexResource {
+
+    @GetMapping(produces = {"text/html"})
+    public String index(Model model) {
+
+        return "indexresource";
+   }
+
+
+
+
+
+
+    
+}
